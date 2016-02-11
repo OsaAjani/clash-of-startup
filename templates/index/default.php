0e7f8881d31b0b97eafb404fc4dfb5484d9c7a1d
OsaAjani <pierre.lin@free.fr>
2016-02-11 10:47:29 +0100

    add some bulshit marketing

--- templates/index/default.php
+++ templates/index/default.php
@@ -6,5 +6,41 @@
 	$incs->header('accueil');
 	$incs->carousel();
 ?>
+	<div class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-sm-4">
+				<div class="block-icon">
+					<i class="fa fa-lightbulb-o"></i>
+				</div>
+				<div class="block-body">
+					<h2>Des idées novatrices</h2>
+					<div class="line-subtitle"></div>
+					<p>Clash Of Startup vous offre la possibilité de participer à l'émergence de projets novateurs, inspirants et inspirés.</p>
+				</div>
+				</div>
+				<div class="col-md-4 col-sm-4">
+				<div class="block-icon">
+					<i class="fa fa-thumbs-o-up"></i>
+				</div>
+				<div class="block-body">
+					<h2>Aidez vos projets préférés</h2>
+					<div class="line-subtitle"></div>
+					<p>Votez pour vos startups préférés, donnez votre avis et aider à faire émerger les projets qui vous tiennent à coeur.</p>
+				</div>
+				</div>
+				<div class="col-md-4 col-sm-4">
+				<div class="block-icon">
+					<i class="fa fa-graduation-cap"></i>
+				</div>
+				<div class="block-body">
+					<h2>Amener à redécouvrir</h2>
+					<div class="line-subtitle"></div>
+					<p>Participez au futur de votre école en donnant une visibilité nouvelle et accrue aux étudiants et à leurs idées.</p>
+				</div>
+				</div>
+			</div> <!-- /.row -->
+		</div> <!-- /.container -->
+	</div> <!-- /.section -->
 <?php
 	$incs->footer();
