ac802cd09ecdefb5e5f73ff472b3a2be0f2602b1
Arezki Drir <arezkidrir@htomail.com>
2016-02-11 14:50:45 +0100

    Ajoute bouton réseaux sociaux

--- templates/incs/footer.php
+++ templates/incs/footer.php
@@ -12,7 +12,10 @@
 				Email : <a class="white-link" href="mailto:clashofstartup@gmail.com" title="Nous envoyer un mail">clashofstartup@gmail.com</a><br/>
 			</address>
 			</div>
-			
+			<a target="_blank" href="https://www.facebook.com/Clashofstartup-1013043628768743/?fref=ts"><i class="fa fa-facebook-official fa-3x" id="btn_facebook" ></i></a>
+			<a target="_blank" href="https://twitter.com/ClahsOfStartup"><i id="btn_twitter" class="fa fa-twitter fa-3x" style="margin-left : 10px"></i></a>
+			<a target="_blank" href="https://plus.google.com/u/0/113697809722682603667/posts?hl=fr"><i class="fa fa-google-plus fa-3x" id="btn_gplus" style="margin-left : 10px"></i></a>
+
 		</div><!-- /.row -->
 
 		</div><!-- /.container -->
